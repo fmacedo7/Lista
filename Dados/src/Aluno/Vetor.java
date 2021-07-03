e010ab36c57bba2f89aca4bf5bb689ceb2ec459c
Felizberto Macedo <55799002+fmacedo7@users.noreply.github.com>
2021-07-03 18:37:08 -0300

    Update Vetor.java
    Atualizei a classe vetor e modifiquei alguns métodos

--- Dados/src/Aluno/Vetor.java
+++ Dados/src/Aluno/Vetor.java
@@ -1,27 +1,45 @@
 package Aluno;
 
-public class Vetor{
-	
-	Aluno [] alunos = new Aluno [3];
+public class Vetor {
+	Aluno[] alunos = new Aluno [3];
 	int cont;
 	
 	public Vetor () {
-		int cont=0;
+		//int cont=0;
 	}
+	
+	//Método para adicionar um aluno em uma lista.
 	public boolean adiciona (Aluno novo) {
 		if (cont < 3) {
-			Aluno[cont] = novo;
+			alunos[cont] = novo;
 			cont++;
 			return true;
 		}
 		return false;
 	}
+	
+	//Método para buscar um aluno.
+	public boolean buscar (int i) {
+		if (i >= 0 && i < cont) {
+			return true;
+		}
+		return false;
+	}
+	
+	//Método para remover um aluno da lista.
 	public boolean remove (int i) {
 		return false;
 	}
+	
+	//Método para exibir um aluno.
 	public Aluno getAlunos(int i) {
-		
+		if (buscar (i)) {
+			return alunos[i];
+		}
+		return null;
 	}
+	
+	//Método para exibir a lista de alunos.
 	public void getLista() {
 		
 	}

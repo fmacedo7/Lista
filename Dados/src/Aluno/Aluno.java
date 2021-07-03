a7a9beaa4f58b2e167e39dcd7a0e978c33f67adb
Felizberto Macedo <55799002+fmacedo7@users.noreply.github.com>
2021-07-03 18:35:51 -0300

    Update Aluno.java
    Atualizei a classe aluno

--- Dados/src/Aluno/Aluno.java
+++ Dados/src/Aluno/Aluno.java
@@ -1,27 +1,38 @@
 package Aluno;
 
-public class Aluno{
-	String Nome;
-	double nota;
-	
-	public Aluno(String Nome, double nota) {
-		setNome(Nome);
-		setNota(nota);
-	}
-	public String getNome () {   //MÈtodo para obter um nome.
-		return Nome;
-	}
-	public void setNome (String nome) {  //MÈtodo para fixar um nome obtido.
-		this.Nome = nome;
-	}
-	public double getNota () {    //MÈtodo para obter uma nota.
-		return nota;
-	}
-	public void setNota(double nota) {   //MÈtodo para fixar uma nota obtida.
-		this.nota = nota;
-	}
-	
-	public String toString() {
-		return "(Nome: "+getNome()+", "+getNota()+")";  //MÈtodo para retornar um nome e uma nota obtida.
+public class Aluno {
+		String Nome;
+		double nota;
+		
+		public Aluno(String Nome, double nota) {
+			setNome(Nome);
+			setNota(nota);
+		}
+		public String getNome () {   //M√©todo para obter um nome.
+			return Nome;
+		}
+		public void setNome (String nome) {  //M√©todo para fixar um nome obtido.
+			this.Nome = nome;
+		}
+		public double getNota () {    //M√©todo para obter uma nota.
+			return nota;
+		}
+		public void setNota(double nota) {   //M√©todo para fixar uma nota obtida.
+			this.nota = nota;
+		}
+		
+		public String toString() {
+			return "(Nome: "+getNome()+", "+getNota()+")";  //M√©todo para retornar um nome e uma nota obtida.
+		}
+		
+	public static void main(String[] args) {
+		Aluno novo = new Aluno ("Ad√£o", 10.0);
+		Aluno novo1 = new Aluno ("Eva", 9.0);
+		Aluno novo2 = new Aluno ("Abel", 8.9);
+		
+		Vetor alunos = new Vetor();
+		alunos.adiciona(novo);
+		alunos.adiciona(novo1);
+		alunos.adiciona(novo2);
 	}
 }
